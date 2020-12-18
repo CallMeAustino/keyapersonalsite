b1f9b3ac5d65bdbe38e0427b2877c2469c2ef609
Austin <austin.hokun.wong@gmail.com>
2020-12-18 13:24:38 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+library.add(fas,fab,far);
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello!</h1>
-    </div>
+    <>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path='/' exact />
+          </Switch>
+        </Router>
+      </div>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,59 @@
-import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+import { Button } from './Button.js';
+import { useEffect} from 'react';
 
-function navbar() {
+function Navbar() {
+
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+    
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true);
+        }
+    }
+
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton);
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        KEYA  
+                        <FontAwesomeIcon style={{marginLeft: 15 }}icon={['fas', 'paint-brush']}/>
+                    </Link>
+                    <div className='menu-icon'>
+                        <FontAwesomeIcon icon={click ? ['fas', 'times'] : ['fas', 'bars']} onClick={handleClick}/>
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/about' className='nav-links' onClick={closeMobileMenu}>About</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/artwork' className='nav-links' onClick={closeMobileMenu}>Artwork</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/experiences' className='nav-links' onClick={closeMobileMenu}>Experiences</Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonStyle='btn--outline'>Commission</Button>} 
+                </div>
+            </nav>    
+        </>
     )
 }
 
-export default navbar
+export default Navbar
